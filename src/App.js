9c8e320a89604714895ea98a03f676e4cbdd6a60
Wakatabi <t_wakatabi@uluru.jp>
2019-09-20 18:05:12 +0900

    componentでpropsを試してみる

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -13,9 +13,34 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <Welcome style={msg1} name="Taro" />
+          <Welcome style={msg2} name="Jiro" />
+        </div>
       </div>
     );
   }
+}
+
+const msg1 = {
+  fontSize: "20pt",
+  fontWeight: "bold",
+  padding: "10px",
+  color: "white",
+  backgroundColor: "darkblue",
+  border: "double 5px magenta"
+};
+
+const msg2 = {
+  fontSize: "16pt",
+  fontWeight: "bold",
+  color: "white",
+  padding: "10px",
+  backgroundColor: "cyan"
+};
+
+function Welcome(props) {
+  return <p style={props.style}>Hello React!! Welcome, {props.name}</p>;
 }
 
 export default App;
