463d921dd148bc8638ab992e49b00a99c7ddbb1c
zoku <>
2019-09-22 03:36:09 +0900

    クラスコンポーネントを組み合わせてみる

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Rect from "./Rect";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>React</h1>
+        <p>{this.title}</p>
+        <p>{this.message}</p>
+        <Rect x="50" y="150" w="150" h="150" c="cyan" r="50" />
+        <Rect x="150" y="200" w="150" h="150" c="magenta" r="75" />
+        <Rect x="100" y="250" w="150" h="150" c="black" r="25" />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App title="App" message="This is App Component" />,
   document.getElementById('root')
 );
