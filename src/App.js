491ca158fc1e69739863a369156d0481143beb88
zoku <>
2019-09-22 03:40:43 +0900

    Appをデフォルトから変更

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React</h1>
+        <p>{this.title}</p>
+        <p>{this.message}</p>
       </div>
     );
   }
