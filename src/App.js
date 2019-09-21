69bc45cf9470900bd6087748368480846700d65d
zoku <>
2019-09-22 04:02:18 +0900

    stateを表示、操作してみる

--- src/App.js
+++ src/App.js
@@ -2,18 +2,49 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  msgStyle = {
+    fontSize: "24px",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  };
+
+  btnStyle = {
+    fontSize: "20px",
+    padding: "0px 10px"
+  };
+
   constructor(props) {
-    super();
-    this.title = props.title;
-    this.message = props.message;
+    super(props);
+    this.state = {
+      counter: 0,
+      msg: "Hello State."
+    };
+    setInterval(() => {
+      this.setState(state => ({
+        msg: state.msg + "!"
+      }));
+    }, 1000);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e) {
+    this.setState(state => ({
+      counter: state.counter + 1,
+      msg: "count: " + state.counter
+    }));
   }
 
   render() {
     return (
       <div className="App">
         <h1>React</h1>
-        <p>{this.title}</p>
-        <p>{this.message}</p>
+        <p style={this.msgStyle}>{this.state.msg}</p>
+        <p style={this.msgStyle}>{this.props.msg}</p>
+        <button style={this.btnStyle} onClick={this.doAction}>
+          click
+        </button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
-  <App title="App" message="This is App Component" />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App msg="Hello Props." />, document.getElementById("root"));
