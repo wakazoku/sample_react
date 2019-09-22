8a60643dd89c5d45dc365106d67e34fbec15be61
zoku <>
2019-09-22 17:30:49 +0900

    リスト表示コンポーネント

--- src/App.js
+++ src/App.js
@@ -2,67 +2,83 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  msgStyle1 = {
-    fontSize: "24pt",
+  data = [
+    "This is list sample.",
+    "これはリストのサンプルです。",
+    "配列をリストに変換します。"
+  ];
+  msgStyle = {
+    fontSize: "20pt",
     color: "#900",
     margin: "20px 0px",
-    padding: "5px",
-    borderBottom: "2px solid #900"
-  };
-
-  msgStyle2 = {
-    fontSize: "24pt",
-    color: "white",
-    backgroundColor: "#900",
-    margin: "20px 0px",
-    padding: "5px",
-    borderBottom: "2px solid #900"
-  };
-
-  btnStyle = {
-    fontSize: "20px",
-    padding: "0px 10px"
+    padding: "5px"
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
-      msg: "Hello State.",
-      flg: true
+      list: this.data
     };
-    setInterval(() => {
-      this.setState(state => ({
-        msg: state.msg + "!"
-      }));
-    }, 1000);
-    this.doAction = this.doAction.bind(this);
-  }
-
-  doAction(e) {
-    this.setState(state => ({
-      counter: state.counter + 1,
-      msg: "count: " + state.counter,
-      flg: !state.flg
-    }));
   }
 
   render() {
     return (
       <div className="App">
         <h1>React</h1>
-        <p style={this.msg1Style}>{this.props.msg}</p>
-        <div>
-          {this.state.flg ? (
-            <p style={this.msgStyle1}>count: {this.state.msg}</p>
-          ) : (
-            <p style={this.msgStyle2}>{this.state.msg}です。</p>
-          )}
-        </div>
-        <button style={this.btnStyle} onClick={this.doAction}>
-          click
-        </button>
+        <h2 style={this.msgStyle}>show list.</h2>
+        <List title="サンプル・リスト" data={this.data} />
       </div>
+    );
+  }
+}
+
+class List extends Component {
+  number = 1;
+
+  title = {
+    fontSize: "20pt",
+    fontWeight: "bold",
+    color: "blue"
+  };
+
+  render() {
+    let data = this.props.data;
+    return (
+      <div>
+        <p style={this.title}>{this.props.title}</p>
+        <ul>
+          {data.map(item => {
+            return (
+              <Item number={this.number++} value={item} key={this.number} />
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Item extends Component {
+  li = {
+    listStyleType: "square",
+    fontSize: "16pt",
+    color: "#06",
+    margin: "0px",
+    padding: "0px",
+    textAlign: "left"
+  };
+
+  num = {
+    fontWeight: "bold",
+    color: "red"
+  };
+
+  render() {
+    return (
+      <li style={this.li}>
+        <span style={this.num}>[{this.props.number}]</span>
+        {this.props.value}
+      </li>
     );
   }
 }
