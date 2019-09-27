d763ea5a5d425dc1cae44c5dbe8e27bb385e8625
Wakatabi <t_wakatabi@uluru.jp>
2019-09-27 18:14:34 +0900

    formの値を表示する

--- src/App.js
+++ src/App.js
@@ -2,18 +2,56 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  input = "";
+
+  msgStyle = {
+    fontSize: "20pt",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px"
+  };
+
+  inputStyle = {
+    fontSize: "12px",
+    padding: "5px"
+  };
+
   constructor(props) {
-    super();
-    this.title = props.title;
-    this.message = props.message;
+    super(props);
+    this.state = {
+      message: "type your name: "
+    };
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
+  }
+
+  doChange(event) {
+    this.input = event.target.value;
+  }
+
+  doSubmit(event) {
+    this.setState({
+      message: "Hello," + this.input + "!!"
+    });
+    event.preventDefault();
   }
 
   render() {
     return (
       <div className="App">
         <h1>React</h1>
-        <p>{this.title}</p>
-        <p>{this.message}</p>
+        <h2>{this.state.message}</h2>
+        <form onSubmit={this.doSubmit}>
+          <label>
+            <span style={this.inputStyle}>Message:</span>
+            <input
+              type="text"
+              style={this.inputStyle}
+              onChange={this.doChange}
+            />
+          </label>
+          <input type="submit" style={this.inputStyle} value="click" />
+        </form>
       </div>
     );
   }
