8ae8ce81b7c7bf8a4c0e2c450dc15a00864d89ee
zoku <>
2019-09-29 10:23:24 +0900

    contextとprovider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super();
-    this.title = props.title;
-    this.message = props.message;
-  }
+let data = {
+  title: "Title",
+  message: "this is sample message."
+};
+
+const SampleContext = React.createContext(data);
+
+export default class App extends Component {
+  newdata = {
+    title: "新しいタイトル",
+    message: "これは新しいメッセージです。"
+  };
 
   render() {
     return (
       <div className="App">
-        <h1>React</h1>
-        <p>{this.title}</p>
-        <p>{this.message}</p>
+        <h1>Context</h1>
+        <Title />
+        <Message />
+        <SampleContext.Provider value={this.newdata}>
+          <Title />
+          <Message />
+        </SampleContext.Provider>
+        <Title />
+        <Message />
       </div>
     );
   }
 }
 
-export default App;
+class Title extends Component {
+  static contextType = SampleContext;
+
+  render() {
+    return (
+      <div>
+        <h2>{this.context.title}</h2>
+      </div>
+    );
+  }
+}
+
+class Message extends Component {
+  static contextType = SampleContext;
+
+  render() {
+    return (
+      <div>
+        <h2>{this.context.message}</h2>
+      </div>
+    );
+  }
+}
