9cccbcbb028778ef4da11c181857f10982ef5d51
Wakatabi <t_wakatabi@uluru.jp>
2019-10-01 17:07:07 +0900

    reduxを使ってメモ帳を作る

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./App.css";
+import Memo from "./memo/Memo";
+import AddForm from "./memo/AddForm";
+import FindForm from "./memo/FindForm";
+import DelForm from "./memo/DelForm";
 
 class App extends Component {
-  constructor(props) {
-    super();
-    this.title = props.title;
-    this.message = props.message;
-  }
+  td = {
+    width: "250px"
+  };
 
   render() {
     return (
-      <div className="App">
-        <h1>React</h1>
-        <p>{this.title}</p>
-        <p>{this.message}</p>
+      <div>
+        <h1>Memo</h1>
+        <AddForm />
+        <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td style={this.td}>
+                <FindForm />
+              </td>
+              <td style={this.td}>
+                <DelForm />
+              </td>
+            </tr>
+            <Memo />
+          </tbody>
+        </table>
       </div>
     );
   }
 }
-
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+import MemoStore from "./memo/Store";
 
 ReactDOM.render(
-  <App title="App" message="This is App Component" />,
+  <Provider store={MemoStore}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
