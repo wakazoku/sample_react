274d49de41b7fa9904157f0faafcbf4b3604aede
Wakatabi <t_wakatabi@uluru.jp>
2019-10-03 17:39:52 +0900

    カウンターアプリを永続化

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super();
-    this.title = props.title;
-    this.message = props.message;
-  }
-
   render() {
     return (
       <div className="App">
-        <h1>React</h1>
-        <p>{this.title}</p>
-        <p>{this.message}</p>
+        <h1>Redux</h1>
+        <Message />
+        <Button />
       </div>
     );
   }
 }
+// ストアのコネクト
+App = connect()(App);
+
+// メッセージ表示のコンポーネント
+class Message extends Component {
+  style = {
+    fontSize: "20pt",
+    padding: "20px 5px"
+  };
+
+  render() {
+    return (
+      <p style={this.style}>
+        {this.props.message}: {this.props.counter}
+      </p>
+    );
+  }
+}
+// ストアのコネクト
+Message = connect(state => state)(Message);
+
+// ボタンのコンポーネント
+class Button extends Component {
+  style = {
+    fontSize: "16pt",
+    padding: "5px 10px"
+  };
+
+  constructor(props) {
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(event) {
+    if (event.shiftKey) {
+      this.props.dispatch({ type: "DECREMENT" });
+    } else if (event.altKey) {
+      this.props.dispatch({ type: "RESET" });
+    } else {
+      this.props.dispatch({ type: "INCREMENT" });
+    }
+  }
+
+  render() {
+    return (
+      <button style={this.style} onClick={this.doAction}>
+        click
+      </button>
+    );
+  }
+}
+// ストアのコネクト
+Button = connect()(Button);
 
 export default App;
